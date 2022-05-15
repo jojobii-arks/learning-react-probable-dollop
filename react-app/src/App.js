cdc294869e9a3d8aa6b0d9175b961ccf56fc0263
Joseph Bravo <josephangelobravo@gmail.com>
2022-05-15 17:33:08 +0000

    created hot button

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,37 @@
+import { useState, useEffect } from 'react';
+
 function App() {
+  const [count, setCount] = useState(0);
+  const [buttonColor, setButtonColor] = useState('bg-white');
+
+  function increment() {
+    setCount(count + 1);
+  }
+
+  useEffect(() => {
+    if (count >= 25) {
+      setButtonColor('bg-pink-600');
+    } else if (count >= 20) {
+      setButtonColor('bg-green-500');
+    } else if (count >= 15) {
+      setButtonColor('bg-blue-500');
+    } else if (count >= 10) {
+      setButtonColor('bg-purple-500');
+    } else if (count >= 5) {
+      setButtonColor('bg-red-500');
+    } else setButtonColor('bg-white');
+    console.log(count);
+  }, [count]);
+
   return (
-    <div class="flex justify-center items-center min-h-screen bg-slate-400">
-      <h1 class="font-extrabold text-3xl text-center bg-white rounded-xl p-8">
-        This is a react app...
-      </h1>
+    <div className="flex min-h-screen items-center justify-center bg-pink-200">
+      <button
+        onClick={increment}
+        className={`${buttonColor} w-40 rounded-full px-12 py-4 text-4xl
+        font-extrabold shadow-md transition-colors duration-300
+        active:translate-y-1 active:shadow-none`}>
+        {count}
+      </button>
     </div>
   );
 }

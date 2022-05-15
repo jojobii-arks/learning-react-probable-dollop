49e50ca08c04c2dca3e18348becf387502203fe0
Joseph Bravo <josephangelobravo@gmail.com>
2022-05-15 16:34:51 +0000

    added react-app installed tailwind

---
+++ react-app/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div class="flex justify-center items-center min-h-screen bg-slate-400">
+      <h1 class="font-extrabold text-3xl text-center bg-white rounded-xl p-8">
+        This is a react app...
+      </h1>
+    </div>
+  );
+}
+
+export default App;
